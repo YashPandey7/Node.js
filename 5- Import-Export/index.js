a553b86db9577cbee335f5395a41c89563bfdaf9
Yash Pandey <yashdevesh10@gmail.com>
2023-04-15 00:59:51 +0530

    Adding files

--- 5- Import-Export/index.js
+++ 5- Import-Export/index.js
@@ -1,4 +1,5 @@
-const add = require('./oper');
+const {add, sub, name} = require('./oper');
 
 console.log(add(5, 5));
-// console.log(name);
+console.log(sub(5,1));
+console.log(name);
--- 5- Import-Export/oper.js
+++ 5- Import-Export/oper.js
@@ -2,6 +2,10 @@ const add = (a,b) => {
     return a + b ;
 };
 
+const sub = (a,b) => {
+    return a - b;
+}
+
 const name = "yash";
 
-module.exports = add ;
+module.exports = {add, sub, name} ;
