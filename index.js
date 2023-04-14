6f719a2aac30ed241ad920d07df0bbf0e37cba36
Yash Pandey <yashdevesh10@gmail.com>
2023-04-14 19:55:50 +0530

    Adding files

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const fs = require("fs");
 // fs.writeFileSync("read.txt","Thapa Technical, Welcomes you all!");
 // fs.appendFileSync("read.txt", " Yash Pandey Here.");
 
-const buf_data = fs.readFileSync("read.txt");
-// console.log(buf_data);
-const org_data = buf_data.toString();
-console.log(org_data);
+// const buf_data = fs.readFileSync("read.txt");
+// // console.log(buf_data);
+// const org_data = buf_data.toString();
+// console.log(org_data);
 
-fs.renameSync("read.txt", "readwrite.txt");
+// fs.renameSync("read.txt", "readwrite.txt");
+
+// fs.mkdirSync("Lec");
+
+// fs.writeFileSync("Lec/Bio.txt", "Hello Everyone");    
+// fs.appendFileSync("Lec/Bio.txt", " -Dark Batman");
+
+// const buf_data = fs.readFileSync('Lec/Bio.txt');
+// // console.log(buf_data);
+// const org_data = buf_data.toString();
+// console.log(org_data);
+
+// const data = fs.readFileSync('Lec/Bio.txt',"utf-8");
+// console.log(data);
+
+fs.renameSync("Lec/Bio.txt", "Lec/myBio.txt");
