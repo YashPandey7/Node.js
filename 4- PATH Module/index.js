97d29f8271ca8915ed686e40240a76a5a2974e27
Yash Pandey <yashdevesh10@gmail.com>
2023-04-15 00:17:44 +0530

    Adding files

---
+++ 4- PATH Module/index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+console.log(path.dirname("D:/Programming/Node/4- PATH Module/index.js"));
+console.log(path.extname("D:/Programming/Node/4- PATH Module/index.js"));
+console.log(path.basename("D:/Programming/Node/4- PATH Module/index.js"));
+
+console.log(path.parse("D:/Programming/Node/4- PATH Module/index.js"));
