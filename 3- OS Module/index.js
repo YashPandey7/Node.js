4ef1692285e7667d58e0bc3d6cd4a86b42930978
Yash Pandey <yashdevesh10@gmail.com>
2023-04-15 00:14:16 +0530

    Adding files

---
+++ 3- OS Module/index.js
@@ -0,0 +1,10 @@
+const os = require("os");
+
+console.log(os.arch());
+console.log(os.platform());
+
+const freeMemory = os.freemem();
+console.log(`${freeMemory/1024/1024/1024}`);
+
+const totalMemory = os.totalmem();
+console.log(`${totalMemory/1024/1024/1024}`);
