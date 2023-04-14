c4a51955fac33f39b02f156635eff85ff8f0c8c4
Yash Pandey <yashdevesh10@gmail.com>
2023-04-14 22:15:29 +0530

    Adding files

---
+++ 2-Async/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+// File Creation 
+// fs.writeFile("Bio.txt","Hello Everyone", (err)=> {
+//     console.log("File Created");
+//     console.log(err);
+// });
+
+// File Append 
+// fs.appendFile("Bio.txt", " - Dark Batman", (err)=> {
+//     console.log("Text added successfully");
+//     console.log(err);
+// });
