7fabcd73e63ad860d4dbb107ed6692d7b2373968
Yash Pandey <yashdevesh10@gmail.com>
2023-04-15 11:46:22 +0530

    Adding files

--- 7- Module_Wrapper/index.js
+++ 7- Module_Wrapper/index.js
@@ -1,2 +1,11 @@
 console.log(__filename);
 console.log(__dirname);
+
+(function(exports, require, module, __filename, __dirname) {
+    const a="Yash";
+    const b="Pandey"
+    console.log(a);
+    // module.exports = {a,b};
+})();
+
+// console.log(a);
