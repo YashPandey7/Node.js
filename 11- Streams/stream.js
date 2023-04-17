9c8b3e4b3ac8e07bcf4043d44a34bf67397279a4
Yash Pandey <yashdevesh10@gmail.com>
2023-04-18 01:02:42 +0530

    Adding files

--- 11- Streams/stream.js
+++ 11- Streams/stream.js
@@ -3,13 +3,30 @@ const http = require('http');
 
 const server = http.createServer();
 
-server.on("request", (req, res) => {
-    fs.readFile("API.json", "utf-8", (err, data) => {
-        if(err)
-        {
-            console.log(err);
-        }
-        res.end(data);
+// server.on("request", (req, res) => {
+//     fs.readFile("API.json", "utf-8", (err, data) => {
+//         if(err)
+//         {
+//             console.log(err);
+//         }
+//         res.end(data);
+//     });
+// });
+
+server.on('request', (req, res) => {
+    const rstream = fs.createReadStream("API.json");
+    rstream.on("data", (chunkdata) => {
+        res.write(chunkdata);
+    });
+
+    rstream.on("end", ()=> {
+        res.end();
+    });
+
+    rstream.on("error", (err)=>{
+        console.log(err);
+        // res.writeHead(404, {"content-type": "html/text"});
+        res.end("<h1>404! Page not found</h1>");
     });
 });
 
