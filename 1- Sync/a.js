58e478bb71ca185fa77eb3735ed1be7f3c281da9
Yash Pandey <yashdevesh10@gmail.com>
2023-05-30 12:02:21 +0530

    Adding merge sort

---
+++ 1- Sync/a.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+// fs.writeFileSync("a_read.txt", "Hello just for revision");
+
+// const buf_data = fs.readFileSync("a_read.txt");
+// const org_data = buf_data.toString();
+// console.log(org_data);
+
+const data = fs.readFileSync("a_read.txt", "utf-8");
+console.log(data);
