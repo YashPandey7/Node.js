7da59a2863f6ce69c09252cca5c1ebfac38446af
Yash Pandey <yashdevesh10@gmail.com>
2023-05-30 13:19:17 +0530

    adding files

--- 8- httpserver/a.js
+++ 8- httpserver/a.js
@@ -1,25 +1,25 @@
-const http = require("http");
+const fs = require("fs");
+const http = require('http');
 
-const server = http.createServer((req,res) => {
-    if(req.url == "/")
-    {
-        res.end("Home Page");
-    }
-    else if(req.url == "/about")
-    {
-        res.end("About Page");
-    }
-    else if(req.url == "/contact")
-    {
-        res.end("Contact Page");
-    }
-    else{
-        res.writeHead(404, {"content-type" : "text/html"});
-        res.end("<h1>404 Error!! Page not found</h1>")
-    }
-    
-});
+fs.readFile("API.json", "utf-8", (err, data)=>{
+    // console.log(data);
+    const org_data = JSON.parse(data);
+    // console.log(org_data[0].name);
+    const server = http.createServer((req,res)=> {
+        if(req.url == "/"){
+            res.end(org_data[0].name);
+        }
+        else if(req.url == "/about")
+        {
+            res.end(org_data[1].name);
+        }
+        else {
+            res.writeHead(404, {"content-type":"text/html"});
+            res.end("<h1>404 Page not found</h1>");
+        }
+    });
 
-server.listen(8000, "127.0.0.1", () => {
-    console.log("listening to port no. 8000");
+    server.listen(8000, "127.0.0.1", ()=>{
+        console.log("Staring server at port no. 8000 ");
+    });
 });
