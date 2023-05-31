56c35ab46a36f07e55f8caf1860eaf834362739c
Yash Pandey <yashdevesh10@gmail.com>
2023-05-31 13:25:20 +0530

    adding files

--- 13- Express/index.js
+++ 13- Express/index.js
@@ -3,7 +3,9 @@ const app = express();
 
 // app.get(route, callback)
 app.get("/", (req, res) => {
-    res.send("Welcome to Home Page");
+    res.write("<h1>Welcome to Home Page</h1>");
+    res.write("<h2>Welcome to Home page again</h2>");
+    res.send();
 });
 
 app.get("/about", (req, res) => {
@@ -12,6 +14,22 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
     res.send("Welcome to Contact Page");
+});
+
+app.get("/temp", (req, res)=> {
+    res.json([{
+        "name": "Amit",
+        "age": 20,
+    },
+    {
+        "name": "Saurav",
+        "age": 22,
+    },
+    {
+        "name": "Yash",
+        "age": 22,
+    },
+    ]);
 });
 
 app.listen(8000, () => {
