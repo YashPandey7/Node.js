b2d086f2eb9bfae36cc0dd98fe9fbd58a8e9b3f2
Yash Pandey <yashdevesh10@gmail.com>
2023-06-01 11:52:12 +0530

    adding files

--- Express.js/3- Viewengine.js
+++ Express.js/3- Viewengine.js
@@ -3,7 +3,17 @@ const express = require("express");
 const app = express();
 
 const staticPath = path.join(__dirname, "/public");
-app.use(express.static(staticPath));
+// app.use(express.static(staticPath));
+
+// To set the view engine
+app.set("view engine", "hbs");
+
+// Template engine route
+app.get("/", (req, res)=> {
+    res.render('index', {
+        Username: "Yash",
+    });
+});
 
 app.get("/", (req, res)=> {
     res.send("Home page");
