d4a3e4c18520d99085d2ee322f4b739b426a2ff6
Yash Pandey <yashdevesh10@gmail.com>
2023-08-03 01:27:54 +0530

    Adding files

--- New folder/express/3-index.js
+++ New folder/express/3-index.js
@@ -5,6 +5,9 @@ const hbs = require("hbs");
 
 const templatePath = path.join(__dirname, "/template/views");
 const partialPath = path.join(__dirname, "/template/partials");
+const cssPath = path.join(__dirname, "/template/public");
+
+app.use(express.static(cssPath));
 
 app.set("view engine", "hbs");
 app.set("views", templatePath);
@@ -13,6 +16,24 @@ hbs.registerPartials(partialPath);
 app.get("/", (req, res) => {
     res.render("3_index", {
         Username : "Yash",
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("3_about", {
+        Username : "batman",
+    });
+});
+
+app.get("/about/*", (req, res) => {
+    res.render("3_404", {
+        err : "About us page not found!!",
+    });
+});
+
+app.get("/*", (req, res) => {
+    res.render("3_404", {
+        err: `Page Not Found!`,
     });
 });
 
