31811aa314759e9dd787a8f11447b93fc2b31e54
Yash Pandey <yashdevesh10@gmail.com>
2023-08-08 01:32:29 +0530

    Adding Mern Backend

--- 17- Mern Backend/src/app.js
+++ 17- Mern Backend/src/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 8000;
+require("./db/conn");
+
+const static_path = path.join(__dirname, "../public");
+app.use(express.static(static_path));
+app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-    res.send("Hello.... Home Page");
+    // res.send("Hello.... Home Page");
+    res.render("index", {
+        username : "Yash"
+    });
 });
 
 app.listen(port, () => {
---
+++ 17- Mern Backend/src/db/conn.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/WebRegistration")
+.then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(`Failed To connect to MongoDB ${err}`);
+})
