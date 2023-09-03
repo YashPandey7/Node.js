4e405a935ad98ef00ac437a52f5cbd80b688f2cc
Yash Pandey <yashdevesh10@gmail.com>
2023-09-03 19:36:17 +0530

    Adding Files

--- 17- Mern Backend/src/app.js
+++ 17- Mern Backend/src/app.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 8000;
 require("./db/conn");
 const RegisterModel = require("./models/register");
 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
@@ -48,6 +51,8 @@ app.post("/register", async(req, res) => {
                 confirmpassword : cpassword
             });
 
+            const token = await registerCustomer.generateAuthToken();
+
             const registered = await registerCustomer.save();
             res.status(201).render("login");
         }
@@ -70,8 +75,11 @@ app.post("/login", async(req, res) => {
         const password = req.body.password;
 
         const useremail = await RegisterModel.findOne({email:email});
-        console.log(useremail.password);
-        if(useremail.password === password){
+
+        const ismatch = await bcrypt.compare(password, useremail.password);
+        // console.log(useremail.password);
+
+        if(ismatch){
             res.status(201).render("loggedin",{
                 firstname : useremail.firstname,
                 lastname : useremail.lastname,
@@ -85,6 +93,31 @@ app.post("/login", async(req, res) => {
         res.status(400).send(`Invalid Email ${err}`);
     }
 });
+
+// const securepassword = async(password) => {
+//     const hashpassword = await bcrypt.hash(password, 8);
+//     console.log(hashpassword);
+
+//     const matchpassword = await bcrypt.compare(password, hashpassword);
+//     console.log(matchpassword);
+// };
+// 
+// securepassword("yash1234");
+
+
+
+// const createToken = async() => {
+//     const token = await jwt.sign({_id : "64f4808c2af6de1ec1e23b97"}, "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ", {
+//         expiresIn : "20 minutes"
+//     });
+//     console.log(token);
+
+//     const userVerify = await jwt.verify(token, "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ");
+//     console.log(userVerify);
+// }
+
+// createToken();
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- 17- Mern Backend/src/models/register.js
+++ 17- Mern Backend/src/models/register.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerSchema = mongoose.Schema({
     firstname : {
@@ -26,8 +28,45 @@ const registerSchema = mongoose.Schema({
     confirmpassword : {
         type : String,
         required : true
-    }
+    },
+    tokens : [{
+        token : {
+            type : String ,
+            required : true
+        }
+    }]
 });
+
+// Generating Tokens
+registerSchema.methods.generateAuthToken = async function () {
+    try{
+        const token = await jwt.sign({_id : this._id.toString()}, "IxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyf");
+
+        this.tokens = this.tokens.concat({token: token});
+        await this.save();
+        console.log(token);
+        return token;
+    }catch(err){
+        resizeBy.send(err);
+        console.log(err);
+    }
+}
+
+
+// Hashing
+registerSchema.pre("save", async function(next) {
+
+    if(this.isModified("password")){
+        // console.log(`Current password is ${this.password}`);
+        this.password = await bcrypt.hash(this.password, 10);
+        // console.log(`Current Hashing Password is ${this.password}`);
+
+        this.confirmpassword = await bcrypt.hash(this.password, 10);
+    }
+
+    next();
+})
+
 
 const Register = new mongoose.model("register", registerSchema);
 
